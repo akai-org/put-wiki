832160d722a15699a56938e8ebad4d252991ceb3
zakhildev <piotr.blecharski02@gmail.com>
2026-01-29 22:27:39 +0100

    remove: Remove # aliastes in favor of single @ alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,12 +8,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '#components': path.resolve(__dirname, './src/components'),
-      '#assets': path.resolve(__dirname, './src/assets'),
-      '#utils': path.resolve(__dirname, './src/utils'),
-      '#hooks': path.resolve(__dirname, './src/hooks'),
-      '#styles': path.resolve(__dirname, './src/styles'),
-      '#pages': path.resolve(__dirname, './src/pages'),
       '@': path.resolve(__dirname, './src'),
     },
   },
