eeb3b61b5b331acd8ac69503634fac5d3d1e5532
Zakhil <piotr.blecharski02@gmail.com>
2026-02-01 20:13:57 +0100

    feat: Configure dependencies (#8)
    Adds and configures dependencies for the front-end:
    - Tailwind CSS
    - Tanstack Router and Query
    - Axios
    - Zod
    - ESLint
    - Storybook
    - Vitest
    - Prettier

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@/styles/tailwind.css';
+import '@/styles/global.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from '@tanstack/react-router';
+import router from '@/lib/router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
