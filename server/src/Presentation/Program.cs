7da3a642bf922533d823840d59f672cd999d9812
Joszkooo <olbrzym012@wp.pl>
2026-02-14 16:25:45 +0100

    Simplifie architecture

--- server/src/Presentation/Program.cs
+++ server/src/Presentation/Program.cs
@@ -1,9 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -15,9 +11,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
