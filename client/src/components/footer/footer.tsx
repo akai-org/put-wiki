83a8df72c92372c41f2ae0e727c7540501a65b8a
Antoni Małecki <antmalecki@gmail.com>
2026-03-20 13:21:36 +0100

    fix: delete socials icons, simplify logo import

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,9 +1,6 @@
 import { footerSections } from './footerSections';
 import '@/styles/global.css';
-
-const getImageUrl = (name: string) => {
-  return new URL(`../../assets/graphics/footer/${name}`, import.meta.url).href;
-};
+import logo from '../../assets/graphics/footer/logo2.jfif';
 
 export const Footer = () => {
   return (
@@ -12,42 +9,11 @@ export const Footer = () => {
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
           <div className=" text-center items-center">
             <div className="flex justify-center items-center">
-              <img
-                src={getImageUrl('logo2.jfif')}
-                className="w-10 h-10"
-                alt="logoOfPolitechnikaPoznanska"
-              />{' '}
+              <img src={logo} className="w-10 h-10" alt="logoOfPolitechnikaPoznanska" />{' '}
             </div>
             <h2>PUT - WIKI</h2>
             Nowoczesne wsparcie dla studentów Politechniki Poznańskiej. Wszystkie informacje w
             jednym miejscu.
-            <br />
-            <div className="flex justify-center items-center">
-              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={getImageUrl('instagramIcon.png')}
-                  className="w-8 h-8"
-                  alt="Instagram"
-                  style={{ margin: '5px' }}
-                />
-              </a>
-              <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={getImageUrl('facebookIcon.png')}
-                  className="w-8 h-8"
-                  alt="Facebook"
-                  style={{ margin: '5px' }}
-                />
-              </a>
-              <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={getImageUrl('linkedinIcon2.png')}
-                  className="w-8 h-8"
-                  alt="LinkedIn"
-                  style={{ margin: '5px' }}
-                />
-              </a>
-            </div>
           </div>
 
           {footerSections.map((section) => (
