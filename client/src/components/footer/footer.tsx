cd266e46eaaaaa3774a4dc9a0b37ab97a0efe8ad
Antoni Małecki <antmalecki@gmail.com>
2026-03-21 12:26:39 +0100

    fix: make footer responsive on mobile device

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className=" text-center items-center">
             <div className="flex justify-center items-center">
               <img src={logo} className="w-10 h-10" alt="logoOfPolitechnikaPoznanska" />{' '}
@@ -18,17 +18,16 @@ export default function Footer() {
 
           {FooterSections.map((section) => (
             <div key={section.id}>
-              <h3>{section.title}:</h3> <br />
+              <h3 className="flex justify-center items-center">{section.title}:</h3>
               <ul>
                 {section.links.map((link, index) => (
-                  <li key={link.label + index}>
+                  <li key={link.label + index} className="flex justify-center items-center">
                     <a href={link.href}> {link.label} </a>
                   </li>
                 ))}
               </ul>
             </div>
           ))}
-          <br />
         </div>
       </div>
     </footer>
