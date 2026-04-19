6c7b1aa6c5b78a96e5934e80030e9117309deb8a
Marcin Słowikowski <marci.slowiko@gmail.com>
2026-04-19 18:29:11 +0200

    init (working login)

---
+++ server/Application/DTOs/UsosUserDto.cs
@@ -0,0 +1,10 @@
+namespace Application.DTOs;
+
+public class UsosUserDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string StudentNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
--- server/Presentation/Program.cs
+++ server/Presentation/Program.cs
@@ -1,5 +1,6 @@
 using Infrastructure.Extensions;
 using Infrastructure.Identity;
+using Infrastructure.Auth;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
@@ -8,6 +9,28 @@ using Presentation.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddMemoryCache();
+builder.Services.Configure<UsosOAuthSettings>(builder.Configuration.GetSection("UsosOAuth"));
+builder.Services.AddHttpClient<UsosOAuthService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder
+    .Services.AddIdentityApiEndpoints<ApplicationUser>(opt =>
+    {
+        opt.User.RequireUniqueEmail = true;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddWebServices();
 
