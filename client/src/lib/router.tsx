f80c56dbb34ef8cd83ada896559110b5a487157a
Wojciech Stuchlak <wojciech.stuchlak@gmail.com>
2026-08-07 16:37:34 +0200

    chore: redo layout, add route, change margin

--- client/src/lib/router.tsx
+++ client/src/lib/router.tsx
@@ -17,7 +17,7 @@ const coursePageRoute = createRoute({
 
 const lecturerPageRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: '/lecturer',
+  path: '/lecturer/$slug',
   component: () => <LecturerPage />,
 });
 
