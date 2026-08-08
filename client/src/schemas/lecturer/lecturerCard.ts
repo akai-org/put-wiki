0cd155a01c37a684feb0a29abd6c54779b421d0f
Wojciech Stuchlak <wojciech.stuchlak@gmail.com>
2026-08-08 23:17:33 +0200

    feat: add lecturer schema modify lecturerCard schema

--- client/src/schemas/lecturer/lecturerCard.ts
+++ client/src/schemas/lecturer/lecturerCard.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const LecturerCardSchema = z.object({
-  title: z.string(),
   name: z.string(),
-  surname: z.string(),
+  title: z.string(),
+  photo_url: z.url(),
 });
 
 export type LecturerCard = z.infer<typeof LecturerCardSchema>;
