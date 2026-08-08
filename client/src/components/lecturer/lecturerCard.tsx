79339a19dbd8e84dff77be97c66d86d3ada6ad65
Wojciech Stuchlak <wojciech.stuchlak@gmail.com>
2026-08-08 16:09:32 +0200

    chore: redo layout to fit the new wireframe

---
+++ client/src/components/lecturer/lecturerCard.tsx
@@ -0,0 +1,3 @@
+export default function LecturerCard() {
+  return <div>janusz</div>;
+}
---
+++ client/src/components/lecturer/lecturerCourseList.tsx
@@ -0,0 +1,10 @@
+import LecturerCourse from './lecturerCourse';
+
+export default function ListLecturerCourses() {
+  return (
+    <div className="flex-col">
+      <LecturerCourse />
+      <LecturerCourse />
+    </div>
+  );
+}
