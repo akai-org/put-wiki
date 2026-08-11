f69fcb1d829c728bfa7cc9ea716cb76811a0532b
Aleksander Nowicki <101459210+Aleks334@users.noreply.github.com>
2026-08-11 20:21:08 +0200

    Revert "feat: degree course page" [skip actions] (#125)
    Revert "feat: degree course page (#75)"
    
    This reverts commit 67d463c64226086c527b41e788d01ec8923fd57c.

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,3 @@
 export { default as HomePage } from './home';
 export { default as NotFoundPage } from './404';
 export { default as CoursePage } from './course';
-export { default as DegreeCoursePage } from './degreeCourse';
