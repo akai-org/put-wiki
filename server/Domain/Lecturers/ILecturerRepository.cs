75f3795560a8f781017f2d7de8c5864f825356f3
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2026-08-12 13:24:53 +0200

    feat: add lecturer repository interface

---
+++ server/Domain/Lecturers/ILecturerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.Lecturers;
+
+public interface ILecturerRepository
+{
+    Task<Lecturer?> GetByIdAsync(string id, CancellationToken cancellationToken = default);
+    void Add(Lecturer lecturer);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
