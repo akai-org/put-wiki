7b2739094eea92f363f53598f573507bd4bc1eac
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2026-08-12 17:09:06 +0200

    feat: add ef core fluent api config for new aggregate

--- server/Infrastructure/AppDbContext.cs
+++ server/Infrastructure/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Domain.AcademicTeachers;
 using Domain.Users;
 
 using Infrastructure.EntitiesConfiguration;
@@ -9,11 +10,13 @@ namespace Infrastructure;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public required DbSet<User> Users { get; set; }
+    public DbSet<AcademicTeacher>? AcademicTeachers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         new UserEntityTypeConfiguration().Configure(modelBuilder.Entity<User>());
+        new AcademicTeacherEntityTypeConfiguration().Configure(modelBuilder.Entity<AcademicTeacher>());
     }
 }
---
+++ server/Infrastructure/EntitiesConfiguration/AcademicTeacherEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.AcademicTeachers;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntitiesConfiguration;
+
+public class AcademicTeacherEntityTypeConfiguration : IEntityTypeConfiguration<AcademicTeacher>
+{
+    public void Configure(EntityTypeBuilder<AcademicTeacher> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.UsosId).IsRequired();
+        builder.HasIndex(x => x.UsosId).IsUnique();
+
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
+
+        builder.Property(x => x.PhoneNumber).HasMaxLength(30);
+        builder.Property(x => x.PhotoUrl).HasMaxLength(2048);
+        builder.Property(x => x.WebsiteUrl).HasMaxLength(2048);
+
+        builder.Property(x => x.Degrees).HasField("_degrees").UsePropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
