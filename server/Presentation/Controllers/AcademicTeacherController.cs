9266ec2373cd7a789968cad1b3556e0364b10e68
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2026-08-12 23:40:00 +0200

    feat: add academic teacher controller

---
+++ server/Presentation/Controllers/AcademicTeacherController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Application.Features.AcademicTeachers.Queries;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AcademicTeacherController(
+    GetAcademicTeacherHandler handler) : BaseApiController
+{
+    [HttpGet("teachers/{id}")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAcademicTeacher(string id, CancellationToken ct)
+    {
+        var result = await handler.ExecuteAsync(new GetAcademicTeacherQuery(new Guid(id)), ct);
+
+        return HandleResult(result);
+    }
+}
