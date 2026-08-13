5077e48392b74bbbd91ee0a27c27936279c02ea3
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2026-08-13 11:58:31 +0200

    refactor: define global naming policy for json properties returned from our api

--- server/Application/Features/AcademicTeachers/Queries/AcademicTeacherDto.cs
+++ server/Application/Features/AcademicTeachers/Queries/AcademicTeacherDto.cs
@@ -1,22 +1,20 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Application.Features.AcademicTeachers.Queries;
+﻿namespace Application.Features.AcademicTeachers.Queries;
 
 public record AcademicTeacherDto(
-    [property: JsonPropertyName("id")] string Id,
-    [property: JsonPropertyName("base_info")] AcademicTeacherBaseInfoDto BaseInfo,
-    [property: JsonPropertyName("contact")] AcademicTeacherContactInfoDto ContactInfo,
-    [property: JsonPropertyName("description")] string? Description
+    string Id,
+    AcademicTeacherBaseInfoDto BaseInfo,
+    AcademicTeacherContactInfoDto ContactInfo,
+    string? Description
     );
 
 public record AcademicTeacherBaseInfoDto(
-    [property: JsonPropertyName("name")] string Name,
-    [property: JsonPropertyName("title")] string Title,
-    [property: JsonPropertyName("photo_url")] string? PhotoUrl
+    string Name,
+    string Title,
+    string? PhotoUrl
     );
 
 public record AcademicTeacherContactInfoDto(
-    [property: JsonPropertyName("email")] string Email,
-    [property: JsonPropertyName("phone")] string? PhoneNumber,
-    [property: JsonPropertyName("website_url")] string? WebsiteUrl
+    string Email,
+    string? PhoneNumber,
+    string? WebsiteUrl
     );
