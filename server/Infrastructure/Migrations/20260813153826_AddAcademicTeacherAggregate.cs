bab1ee66de34ca5c6c8735f1225da8992b146acb
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2026-08-13 17:42:48 +0200

    feat: add new migration

---
+++ server/Infrastructure/Migrations/20260813153826_AddAcademicTeacherAggregate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAcademicTeacherAggregate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AcademicTeachers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UsosId = table.Column<string>(type: "text", nullable: false),
+                    Degrees = table.Column<string[]>(type: "text[]", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    PhotoUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    WebsiteUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true),
+                    Description = table.Column<string>(type: "character varying(2500)", maxLength: 2500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AcademicTeachers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicTeachers_UsosId",
+                table: "AcademicTeachers",
+                column: "UsosId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AcademicTeachers");
+        }
+    }
+}
