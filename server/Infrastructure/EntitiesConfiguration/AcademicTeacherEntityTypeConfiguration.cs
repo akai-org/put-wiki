f3efc704d2357036bbff713caba8f7d531ce17ba
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2026-08-14 14:29:10 +0200

    feat: add migration along with columns constraints changes

--- server/Infrastructure/EntitiesConfiguration/AcademicTeacherEntityTypeConfiguration.cs
+++ server/Infrastructure/EntitiesConfiguration/AcademicTeacherEntityTypeConfiguration.cs
@@ -10,19 +10,43 @@ public class AcademicTeacherEntityTypeConfiguration : IEntityTypeConfiguration<A
     public void Configure(EntityTypeBuilder<AcademicTeacher> builder)
     {
         builder.HasKey(x => x.Id);
-        builder.Property(x => x.UsosId).IsRequired();
-        builder.HasIndex(x => x.UsosId).IsUnique();
-        builder.ComplexProperty(x => x.Slug);
 
-        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
-        builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.UsosId)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasIndex(x => x.UsosId)
+            .IsUnique();
+
+        builder.Property(x => x.Slug)
+            .HasConversion(
+                slug => slug.Value,
+                value => AcademicTeacherSlug.Parse(value))
+            .HasColumnName("slug")
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.HasIndex(x => x.Slug)
+            .IsUnique();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasIndex(x => x.Email)
+            .IsUnique();
 
         builder.Property(x => x.PhoneNumber).HasMaxLength(30);
-        builder.Property(x => x.PhotoUrl).HasMaxLength(2048);
-        builder.Property(x => x.WebsiteUrl).HasMaxLength(2048);
-
+        builder.Property(x => x.PhotoUrl).HasMaxLength(2000);
+        builder.Property(x => x.WebsiteUrl).HasMaxLength(2000);
         builder.Property(x => x.Description).HasMaxLength(2500);
 
-        builder.Property(x => x.Degrees).HasField("_degrees").HasColumnName("degrees").UsePropertyAccessMode(PropertyAccessMode.Field);
+        builder.Property(x => x.Degrees)
+            .HasField("_degrees")
+            .HasColumnName("degrees")
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
     }
 }
