5cbe28af89f59829088d63c8bc11ea2b8665978b
Wojciech Stuchlak <wojciech.stuchlak@gmail.com>
2026-08-16 19:52:42 +0200

    chore: change global css and change homepage

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,19 +1,19 @@
 export default function HomePage() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <h1 className="text-primary font-bold text-5xl">PUT Wiki Home</h1>
+      <h1 className=" font-bold text-5xl">PUT Wiki Home</h1>
 
-      <div className="mt-6 flex flex-col items-center gap-2">
+      <div className="mt-6 p-6 flex flex-col items-center gap-2 bg-primary">
         <a
           href="/course/analiza-i-struktury-danych"
-          className="text-secondary hover:text-primary/80 transition-colors"
+          className="text-primary-foreground transition-colors hover:text-accent"
         >
           Analiza i struktury danych
         </a>
 
         <a
           href="/lecturers/jan-kowalski"
-          className="text-secondary hover:text-secondary/80 transition-colors"
+          className="text-primary-foreground hover:text-primary/80 transition-colors"
         >
           Jan Kowalski
         </a>
