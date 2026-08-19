0dfd17e06e87ade3afd4c47945b7d6d1bfecb74c
Wojciech <wojciech.stuchlak@gmail.com>
2026-08-19 10:05:19 +0200

    feat: add interfaces

--- client/src/components/lecturer/aboutLecturer.tsx
+++ client/src/components/lecturer/aboutLecturer.tsx
@@ -1,4 +1,8 @@
-export default function AboutLecturer(props: { description: string }) {
+interface AboutLecturerProps {
+  description: string;
+}
+
+export default function AboutLecturer(props: AboutLecturerProps) {
   return (
     <div className="flex flex-col p-3 rounded-xl bg-primary text-primary-foreground">
       <p className="text-xl">O prowadzącym</p>
--- client/src/components/lecturer/lecturerCourseList.tsx
+++ client/src/components/lecturer/lecturerCourseList.tsx
@@ -1,7 +1,11 @@
 import type { LecturerCourse } from '@/schemas/lecturer/lecturerCourse';
 import LecturerCourseItem from './lecturerCourseItem';
 
-export default function ListLecturerCourses({ courseList }: { courseList: Array<LecturerCourse> }) {
+interface LecturerCourseListProps {
+  courseList: LecturerCourse[];
+}
+
+export default function ListLecturerCourses({ courseList }: LecturerCourseListProps) {
   return (
     <div className="flex flex-col gap-2 p-4 bg-primary text-primary-foreground rounded-xl">
       <p className="text-2xl">Prowadzone przemdioty</p>
