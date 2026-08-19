c1c81c94da8b0ac0ad16e5c49c2c586e411c6e9d
Wojciech Stuchlak <wojciech.stuchlak@gmail.com>
2026-08-19 02:29:00 +0200

    chore: adjust courseList Component

--- client/src/components/lecturer/lecturerCourseList.tsx
+++ client/src/components/lecturer/lecturerCourseList.tsx
@@ -6,9 +6,9 @@ export default function ListLecturerCourses({ courseList }: { courseList: Array<
     <div className="flex flex-col gap-2 p-4 bg-primary text-primary-foreground rounded-xl">
       <p className="text-2xl">Prowadzone przemdioty</p>
       <div>
-        <p className="font-bold text-xl">2025/2026</p>
+        <p className="font-bold text-xl">2026/2027</p>
         {courseList
-          .filter((c) => c.semestr === 3)
+          .filter((c) => c.semestr === '2026/2027')
           .map((c) => (
             <LecturerCourseItem key={c.nazwa + c.semestr} course={c} />
           ))}
@@ -16,7 +16,7 @@ export default function ListLecturerCourses({ courseList }: { courseList: Array<
       <div>
         <p className="font-bold text-xl">Poprzednie semsetry</p>
         {courseList
-          .filter((c) => c.semestr !== 3)
+          .filter((c) => c.semestr !== '2025/2026')
           .map((c) => (
             <LecturerCourseItem key={c.nazwa} course={c} />
           ))}
