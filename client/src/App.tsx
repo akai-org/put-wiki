e0990a7ae46413ffd3481d4791f5c4174d10d007
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2026-08-23 21:49:00 +0200

    fix: remove old providers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import router from '@/lib/router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/contexts/ThemeProvider';
-import { AuthProvider } from '@/contexts/AuthProvider';
-import { UserProvider } from '@/contexts/UserProvider';
 import { ToastsProvider } from '@/components/ui/ToastsProvider';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { queryClient } from '@/lib/queryClient';
@@ -12,16 +10,12 @@ import { queryClient } from '@/lib/queryClient';
 export function App() {
   return (
     <ThemeProvider>
-      <AuthProvider>
-        <UserProvider>
-          <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-            <ToastsProvider position="top-center" />
-            <TanStackRouterDevtools router={router} />
-            <ReactQueryDevtools />
-          </QueryClientProvider>
-        </UserProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastsProvider position="top-center" />
+        <TanStackRouterDevtools router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
