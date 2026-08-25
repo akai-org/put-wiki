2fdd091e15d9ee7076f47e5d93618129b2479116
Aleksander Nowicki <101459210+Aleks334@users.noreply.github.com>
2026-08-25 20:42:17 +0200

    Update client/src/features/opinions/OpinionLabel.tsx
    Co-authored-by: Wojciech Stuchlak <wojciech.stuchlak@gmail.com>

--- client/src/features/opinions/OpinionLabel.tsx
+++ client/src/features/opinions/OpinionLabel.tsx
@@ -2,7 +2,7 @@ import type { Opinion } from '@/features/opinions/opinionSchema';
 
 export default function OpinionLabel({ userName, content, rating }: Opinion) {
   return (
-    <div className="">
+    <div>
       <h3>{userName}</h3>
       <p>{content}</p>
       <p>Ocena: {rating}</p>
