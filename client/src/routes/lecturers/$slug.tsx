67764fd275db76fc76e3027e65f215e9eec5f3ae
Wojciech Stuchlak <wojciech.stuchlak@gmail.com>
2026-08-28 15:58:45 +0200

    feat: add lectuer's routing

---
+++ client/src/routes/lecturers/$slug.tsx
@@ -0,0 +1,38 @@
+import ContactCard from '@/features/lecturers/components/ContactCard';
+import BaseInfoCard from '@/features/lecturers/components/BaseInfoCard';
+import { createFileRoute, useParams } from '@tanstack/react-router';
+import { AboutCard, ConsultationCard } from '@/features/lecturers';
+import { useLecturerQuery } from '@/features/lecturers/api/useLecturersQuery';
+import TempComp from '@/features/lecturers/components/TempComp';
+
+export const Route = createFileRoute('/lecturers/$slug')({ component: LecturerPage });
+
+function LecturerPage() {
+  const { slug } = useParams({ from: '/lecturers/$slug' });
+  const { data, isLoading, isError } = useLecturerQuery(slug);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+  if (!data) {
+    return <div>Error</div>;
+  }
+
+  return (
+    <div className="mt-4w-full mx-auto max-w-7xl">
+      <div className="grid grid-cols-1 self-stretch md:grid-cols-[7fr_3fr]">
+        <BaseInfoCard {...data.baseInfo} />
+        <ContactCard {...data.contactInfo} />
+      </div>
+      <AboutCard description={data.description} />
+      <div className="grid grid-cols-1 self-stretch md:grid-cols-2">
+        <TempComp />
+        <ConsultationCard />
+      </div>
+    </div>
+  );
+}
